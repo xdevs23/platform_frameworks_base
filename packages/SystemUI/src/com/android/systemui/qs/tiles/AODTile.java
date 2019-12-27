52cafba2e3da14677080c07214b646f4dacc535f
spezi77 <spezi7713@gmx.net>
2019-12-27 12:39:10 +0100

    AOD qs tile: Launch LockscreenDisplaySettings on long click.
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/AODTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/AODTile.java
@@ -68,7 +68,8 @@ public class AODTile extends QSTileImpl<BooleanState> {
 
     @Override
     public Intent getLongClickIntent() {
-        return null;
+        return new Intent().setComponent(new ComponentName(
+            "com.android.settings", "com.android.settings.Settings$LockscreenDisplayActivity"));
     }
 
     @Override
